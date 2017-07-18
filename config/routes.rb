5277de507f9e5e65db9802dbacaa62d7a50af64f
Dumar Ojeda <dumarojeda@gmail.com>
2017-07-18 17:18:48 -0500

    Create Pages controller for static pages and remove filter authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :accounts
   devise_for :customers
   devise_for :admins
+
+  root "pages#show", page: "home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
