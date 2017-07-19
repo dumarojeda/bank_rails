41a28285120ac40438bfda98625cc5052a760638
Dumar Ojeda <dumarojeda@gmail.com>
2017-07-19 00:10:51 -0500

    Add validations to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
 class Customer < ApplicationRecord
   # Validations
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, :document_number, :phone, :city, :email, presence: true
 end
