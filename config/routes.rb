e79695589b87f224b1043cad9f2701b6f7efe98b
Dumar Ojeda <dumarojeda@gmail.com>
2017-07-19 21:27:30 -0500

    Add devise_scope on routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,14 @@ Rails.application.routes.draw do
   resources :customers
   devise_for :admins
 
-  root "pages#show", page: "home"
+  devise_scope :admin do
+    authenticated :admin do
+      root "pages#show", page: "home"
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
